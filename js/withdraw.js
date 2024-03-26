dc90fbb97484c8348e60dd676ed3c3af21d875e9
Md Sabbir Khan <mdsabbirkhan1972@gmail.com>
2024-03-26 22:47:14 +0600

    final again

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,28 +1,50 @@
 document.getElementById('withdraw-btn').addEventListener('click',function(){
-    // first step 
-    const withdrawField = document.getElementById('withdrawField');
-    const withdrawAmountString = withdrawField.value;
-    const withdrawAmount = parseFloat(withdrawAmountString);
-    console.log(withdrawAmount)
-
-    // second step 
-
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const withdrawTotalString = withdrawTotalElement.innerText;
     const withdrawTotal = parseFloat(withdrawTotalString);
+    
 
+    const balanceTotalElement = document.getElementById('balance-total');
+    const prebalanceTotalString = balanceTotalElement.innerText;
+    const prebalanceTotal = parseFloat(prebalanceTotalString);
+
+
+    const withdrawField = document.getElementById('withdrawField');
+    const withdrawAmountString = withdrawField.value;
+    const withdrawAmount = parseFloat(withdrawAmountString);
+    withdrawField.value ='';
+
+    if(withdrawAmount > prebalanceTotal){
+        alert('Insufficient balance');
+        return;
+    }
+
+    // first step 
+
+ 
+    
+
+    if (isNaN(withdrawAmount)) {
+        alert('Please provide a valid number')
+        return
+      }
+
+     
+
+    // second step 
+
+    
     // third step 
-
     const totalWithdraw = withdrawTotal + withdrawAmount;
     withdrawTotalElement.innerText = totalWithdraw;
 
+    
     // 4th step 
-    const balanceTotalElement = document.getElementById('balance-total');
-    const prebalanceTotalString = balanceTotalElement.innerText;
-   const prebalanceTotal = parseFloat(prebalanceTotalString);
-   
+
+
+    
    const balanceTotal = prebalanceTotal - totalWithdraw;
    balanceTotalElement.innerText =balanceTotal;
 
-   withdrawField.value ='';
+
 })
