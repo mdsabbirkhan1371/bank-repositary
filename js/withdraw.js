61f116f1626de359a61f4fbc86e4cb2d15ff25bf
Md Sabbir Khan <mdsabbirkhan1972@gmail.com>
2024-03-26 22:51:33 +0600

    again

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,47 +1,38 @@
 document.getElementById('withdraw-btn').addEventListener('click',function(){
+    // first step
+    const withdrawField = document.getElementById('withdrawField');
+    const withdrawAmountString = withdrawField.value;
+    const withdrawAmount = parseFloat(withdrawAmountString);
+    withdrawField.value =''; 
+
+    // second step 
+
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const withdrawTotalString = withdrawTotalElement.innerText;
     const withdrawTotal = parseFloat(withdrawTotalString);
-    
 
+    // third 
+    
     const balanceTotalElement = document.getElementById('balance-total');
     const prebalanceTotalString = balanceTotalElement.innerText;
     const prebalanceTotal = parseFloat(prebalanceTotalString);
 
-
-    const withdrawField = document.getElementById('withdrawField');
-    const withdrawAmountString = withdrawField.value;
-    const withdrawAmount = parseFloat(withdrawAmountString);
-    withdrawField.value ='';
 
     if(withdrawAmount > prebalanceTotal){
         alert('Insufficient balance');
         return;
     }
 
-    // first step 
-
- 
-    
-
     if (isNaN(withdrawAmount)) {
         alert('Please provide a valid number')
         return
       }
 
-     
-
-    // second step 
-
-    
-    // third step 
+    //  fourth
     const totalWithdraw = withdrawTotal + withdrawAmount;
     withdrawTotalElement.innerText = totalWithdraw;
 
-    
     // 4th step 
-
-
     
    const balanceTotal = prebalanceTotal - totalWithdraw;
    balanceTotalElement.innerText =balanceTotal;
